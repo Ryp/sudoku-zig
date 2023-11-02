0e57208c21c76ff6c51899b1302273316cef4046
Ryp <thibault.schueller@protonmail.com>
2023-11-02 23:12:03 +0300

    bench: add common sudoku grids

--- src/sudoku/bench.zig
+++ src/sudoku/bench.zig
@@ -5,7 +5,9 @@ const sudoku = @import("game.zig");
 const brute_solver = @import("brute_solver.zig");
 
 // NOTE: See https://stackoverflow.com/questions/24682039/whats-the-worst-case-valid-sudoku-puzzle-for-simple-backtracking-brute-force-al
-// "9..8...........5............2..1...3.1.....6....4...7.7.86.........3.1..4.....2.."
+const naive_backtracking_killer = "..............3.85..1.2.......5.7.....4...1...9.......5......73..2.1........4...9";
+const naive_backtracking_killer_2 = "9..8...........5............2..1...3.1.....6....4...7.7.86.........3.1..4.....2..";
+const easy_17_clues = "...8.1..........435............7.8........1...2..3....6......75..34........2..6..";
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -13,7 +15,7 @@ pub fn main() !void {
     var game = try sudoku.create_game_state(allocator, sudoku.GameType{ .regular = .{
         .box_w = 3,
         .box_h = 3,
-    } }, "..............3.85..1.2.......5.7.....4...1...9.......5......73..2.1........4...9");
+    } }, easy_17_clues);
     defer sudoku.destroy_game_state(allocator, &game);
 
     assert(brute_solver.solve(&game, .{}));
