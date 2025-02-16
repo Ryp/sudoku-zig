ba279b7cbac32e2feaf7dbc679dd837fe705f2fd
Thibault Schueller <1625198+Ryp@users.noreply.github.com>
2025-02-16 21:44:23 +0100

    build: add tracy support

--- build.zig
+++ build.zig
@@ -16,12 +16,30 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
-    exe.linkSystemLibrary("c");
+    exe.linkLibC();
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_ttf");
 
     exe.root_module.addAnonymousImport("font_regular", .{ .root_source_file = b.path("res/FreeSans.ttf") });
     exe.root_module.addAnonymousImport("font_bold", .{ .root_source_file = b.path("res/FreeSansBold.ttf") });
+
+    const enable_tracy = b.option(bool, "tracy", "Enable Tracy support") orelse false;
+
+    const exe_options = b.addOptions();
+    exe_options.addOption(bool, "enable_tracy", enable_tracy);
+    exe.root_module.addOptions("build_options", exe_options);
+
+    if (enable_tracy) {
+        const tracy_path = "external/tracy";
+        const client_cpp = "external/tracy/public/TracyClient.cpp";
+        const tracy_c_flags: []const []const u8 = &[_][]const u8{ "-DTRACY_ENABLE=1", "-fno-sanitize=undefined" };
+
+        exe.root_module.addIncludePath(b.path(tracy_path));
+        exe.root_module.addCSourceFile(.{ .file = b.path(client_cpp), .flags = tracy_c_flags });
+
+        exe.linkLibC();
+        exe.linkLibCpp();
+    }
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
