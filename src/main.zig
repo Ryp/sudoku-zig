5703503caf5a57d2fc7b3e2412951dc84f9ce9e4
Thibault Schueller <1625198+Ryp@users.noreply.github.com>
2026-04-05 21:51:00 +0200

    psp: initial port

--- src/main.zig
+++ src/main.zig
@@ -1,90 +1,24 @@
 const std = @import("std");
-const assert = std.debug.assert;
 
-const game = @import("sudoku/game.zig");
-const board_generic = @import("sudoku/board_generic.zig");
-const rules = @import("sudoku/rules.zig");
+const psp = @import("frontend/psp.zig");
 
-const clap = @import("clap.zig");
-const sdl = @import("frontend/sdl.zig");
+const sdk = @import("pspsdk");
+
+pub const panic = sdk.extra.debug.panic; // Import panic handler
+
+pub const std_options_debug_threaded_io: ?*std.Io.Threaded = null;
+pub const std_options_debug_io: std.Io = sdk.extra.Io.psp_io;
+
+pub fn std_options_cwd() std.Io.Dir {
+    return .{ .handle = -1 };
+}
+
+comptime {
+    asm (sdk.extra.module.module_info("Sudoku", .{ .mode = .User }, 1, 0));
+}
 
 pub fn main(init: std.process.Init) !void {
     const allocator = init.gpa;
 
-    const params = comptime clap.parseParamsComptime(
-        \\-h, --help              Display this help and exit.
-        \\-W, --box_width <u32>   Box width for regular sudokus (default: 3)
-        \\-H, --box_height <u32>  Box height for regular sudokus (default: 3)
-        \\-j, --jigsaw <str>      Region indices string for jigsaw sudokus
-        \\--king                  Chess anti-king's constraint
-        \\--knight                Chess anti-knight's constraint
-        \\<str>                   Sudoku string (you can use '.' for empty cells).
-        \\                        Unset this if you want to have a sudoku board generated for you.
-    );
-
-    var diag = clap.Diagnostic{};
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, init.minimal.args, .{
-        .diagnostic = &diag,
-        .allocator = allocator,
-    }) catch |err| {
-        // Report useful error and exit.
-        try diag.reportToFile(init.io, .stderr(), err);
-        return err;
-    };
-    defer res.deinit();
-
-    if (res.args.help != 0) {
-        return clap.helpToFile(init.io, .stderr(), clap.Help, &params, .{});
-    }
-
-    var board_rules: rules.Rules = .{ .type = undefined };
-
-    if (res.args.jigsaw) |jigsaw_string| {
-        const jigsaw_extent = try get_extent_from_jigsaw_string(jigsaw_string);
-
-        board_rules.type = .{
-            .jigsaw = .{
-                .extent = jigsaw_extent,
-                .box_indices_string = jigsaw_string,
-            },
-        };
-    } else {
-        const box_w = res.args.box_width orelse 3;
-        const box_h = res.args.box_height orelse 3;
-
-        board_rules.type = .{ .regular = .{
-            .box_extent = .{ box_w, box_h },
-        } };
-    }
-
-    board_rules.chess_anti_king = res.args.king != 0;
-    board_rules.chess_anti_knight = res.args.knight != 0;
-
-    const board_extent = board_rules.type.extent();
-
-    // Scalarize extent
-    inline for (board_generic.MinExtent..board_generic.MaxExtent + 1) |comptime_extent| {
-        if (board_extent == comptime_extent) {
-            var game_state = try game.State(comptime_extent).init(init.io, allocator, board_rules, res.positionals[0]);
-            defer game_state.deinit(allocator);
-
-            try sdl.execute_main_loop(comptime_extent, &game_state, allocator);
-
-            break;
-        }
-    } else {
-        @panic("Invalid sudoku extent!");
-    }
-}
-
-fn get_extent_from_jigsaw_string(jigsaw_string: []const u8) !u32 {
-    const string_len = jigsaw_string.len;
-
-    for (board_generic.MinExtent..board_generic.MaxExtent + 1) |extent| {
-        if (string_len == extent * extent) {
-            return @intCast(extent);
-        }
-    }
-
-    return error.InvalidJigsawStringLength;
+    try psp.execute_main_loop(init.io, allocator);
 }
